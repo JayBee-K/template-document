1d1f2c459ca364bdd68840ca25db8a4f881e911a
Jay B <killerdrug007@gmail.com>
2021-03-05 20:35:31 +0700

    Check

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	let windowWidth = $(window).width();
 	if (windowWidth < 992) {
-		$('header .navigation > ul').prepend('<li><a href="javascript:void(0)" class="d-flex d-lg-none" id="close-menu"><i class="fas fa-times"></i></a></li>');
-		$("header .navigation > ul > li > ul").each(function (index) {
+		// $('header .header-navigation > ul').prepend('<li><a href="javascript:void(0)" class="d-flex d-lg-none" id="close-menu"><i class="fas fa-times"></i></a></li>');
+		$("header .header-navigation > ul > li > ul").each(function (index) {
 			$(this).prev().attr({
 				"href": "#subMenu" + index,
 				"data-toggle": "collapse"
@@ -16,17 +16,59 @@ $(document).ready(function () {
 	}
 	
 	function callMenu() {
-		if ($('body').hasClass('show-navigation')) {
-			$('body').removeClass('show-navigation');
+		if ($('body').hasClass('show-header-navigation')) {
+			$('body').removeClass('show-header-navigation');
 		} else {
-			$('body').addClass('show-navigation');
+			$('body').addClass('show-header-navigation');
 		}
 	}
 	
-	$(document).on("click", "#hamburger, #close-menu, .header-overlay", function () {
+	$(document).on("click", "header .header-navigation > a, .header-overlay", function () {
 		callMenu();
 	});
 	
+	const myBanner = new Swiper('#swiper-banner', {
+		loop: true,
+		spaceBetween: 50,
+		speed: 250,
+		effect: 'fade',
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		}
+	});
 	
+	$('#slide-sideRight').slick({
+		vertical: true,
+		verticalSwiping: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 500,
+		autoplay: true,
+		autoplaySpeed: 25000,
+		arrows: false,
+	});
 	
-});
+	$('.input-effect').blur(function () {
+		$(this).val() != '' ? $(this).addClass('valid') : $(this).removeClass('valid');
+	});
+	
+	$('.js_remove-notification').click(function (e) {
+		e.stopPropagation();
+		$(this).closest('li').remove();
+	});
+	
+	$('header .header-mobilesearch > a').click(function (e) {
+		if ($('header .header-form').hasClass('show')) {
+			$('header .header-form').removeClass('show');
+		} else {
+			$('header .header-form').addClass('show');
+		}
+	});
+	
+})
